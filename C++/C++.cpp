1b45f6035d615a5a9e412f0a4c906da837fd0b13
Kadir <qadirrys21@gmail.com>
2023-05-09 22:03:26 +0600

    modifide maim cpp file

--- C++/C++.cpp
+++ C++/C++.cpp
@@ -2,17 +2,10 @@
 using namespace std;
 
 int main() {
-    int arr[3][4] = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} };
-    int maxElement = arr[0][0];
+    
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (arr[i][j] > maxElement) {
-                maxElement = arr[i][j];
-            }
-        }
-    }
+    int res = 10;
+    cout << res << endl;
 
-    cout << "Самый большой элемент в массиве: " << maxElement << endl;
     return 0;
 }
